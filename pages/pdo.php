0949b1aaf161dcd3e88eb5beb7441f25776bb28e
Nabira28 <nurulnabira.edu@gmail.com>
2025-06-04 14:19:10 +0800

    update pdo

--- pages/pdo.php
+++ pages/pdo.php
@@ -1,9 +1,9 @@
 <?php
-$host = 'localhost';      // Database server
-$port = '3306';           // Usually 3306 for MySQL (8889 for MAMP)
-$db   = 'parcelsystem';  // Change this to your actual DB name
-$user = 'root';           // Your MySQL username (often 'root' on XAMPP)
-$pass = '';               // Your MySQL password (often empty '' on XAMPP)
+$host = 'localhost';      
+$port = '3306';           
+$db   = 'parcelsystem';  
+$user = 'root';           
+$pass = '';             
 
 $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
