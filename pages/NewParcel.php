0c3d3fcebb009fd17cb12f1dfdf0860eba708def
Amirul Hakimi <223734@student.upm.edu.my>
2025-06-19 12:01:56 +0800

    edit sikit

--- pages/NewParcel.php
+++ pages/NewParcel.php
@@ -136,7 +136,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $parcel_type = $_POST['Parcel_type'];
     $owner = $_POST['Parcel_owner'];
     $image = $_POST['parcel_image'];
-    $imageData = fopen($_FILES[$image]["tmp_name"],'rb');
+    $imageData = fopen($_FILES['parcel_image']["tmp_name"],'rb');
     
     // Generate auto-incrementing parcel ID
     $parcel_id = generateParcelId($pdo);
